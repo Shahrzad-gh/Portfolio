aa6ec14f5ac269871aa9cb46bc401fce7da56e6b
Shahr Zad <gh.shahrzad@gmail.com>
2021-10-05 22:10:06 +0330

    add home

---
+++ src/components/Home/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Home.css";
+function Home() {
+  return (
+    <div className="header" id="home">
+      <div className="banner">
+        <img
+          src="https://image.freepik.com/free-photo/data-technology-blue-background-with-hacker-remixed-media_53876-108534.jpg"
+          alt="headerPic"
+        />
+        <div className="welcome">
+          Hi, my name is Shahrzad and I'm a Web Developer.
+          <br />
+          Let's make ....
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;

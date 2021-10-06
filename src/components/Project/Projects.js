cb4b9f3e316d5ee1f3ccc8ad8305881b581adfab
Shahr Zad <gh.shahrzad@gmail.com>
2021-10-06 17:02:46 +0330

    edit card style

--- src/components/Project/Projects.js
+++ src/components/Project/Projects.js
@@ -4,62 +4,98 @@ import "./Projects.css";
 function Projects() {
   return (
     <>
-      <div className="banner" style={{ height: "50vh" }}>
-        <img src="https://image.freepik.com/free-photo/web-design-concept-with-drawings_1134-77.jpg" />
+      <div className="banner" style={{ height: "50vh" }} id="projects">
+        <img
+          alt="proBanner"
+          src="https://image.freepik.com/free-photo/web-design-concept-with-drawings_1134-77.jpg"
+        />
+        <div className="work-text">PORTFOLIO</div>
       </div>
       <div style={{ padding: "20px" }}>
         <p className="center">My Works</p>
         <div className="projects">
           <div className="pro-content">
-            <div>Title</div>
+            <div className="title-style">Title</div>
             <div>
-              <img src="https://image.freepik.com/free-photo/laptop_23-2148155456.jpg" />
+              <img
+                alt="pro1"
+                src="https://image.freepik.com/free-photo/laptop_23-2148155456.jpg"
+              />
             </div>
-            <div>content</div>
+            <div className="content-style">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Repellat, voluptatibus nisi cum eligendi repudiandae illo aliquid
+              quas ducimus ipsam. Quia?
+            </div>{" "}
           </div>
           <div className="pro-content">
-            <div className="pro-content">
-              <div>Title</div>
-              <div>
-                <img src="https://image.freepik.com/free-photo/web-design-concept-with-drawings_1134-77.jpg" />
-              </div>
-              <div>content</div>
+            <div className="title-style">Title</div>
+            <div>
+              <img
+                alt="pro2"
+                src="https://image.freepik.com/free-photo/web-design-concept-with-drawings_1134-77.jpg"
+              />
             </div>
+            <div className="content-style">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Repellat, voluptatibus nisi cum eligendi repudiandae illo aliquid
+              quas ducimus ipsam. Quia?
+            </div>{" "}
           </div>
           <div className="pro-content">
-            <div className="pro-content">
-              <div>Title</div>
-              <div>
-                <img src="https://image.freepik.com/free-photo/http-www-website-links-search-box-graphic-concept_53876-125066.jpg" />
-              </div>
-              <div>content</div>
+            <div className="title-style">Title</div>
+            <div>
+              <img
+                alt="pro3"
+                src="https://image.freepik.com/free-photo/http-www-website-links-search-box-graphic-concept_53876-125066.jpg"
+              />
             </div>
+            <div className="content-style">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Repellat, voluptatibus nisi cum eligendi repudiandae illo aliquid
+              quas ducimus ipsam. Quia?
+            </div>{" "}
           </div>
           <div className="pro-content">
-            <div className="pro-content">
-              <div>Title</div>
-              <div>
-                <img src="https://image.freepik.com/free-photo/elderly-man-is-using-mobile-phone_53876-30130.jpg" />
-              </div>
-              <div>content</div>
+            <div className="title-style">Title</div>
+            <div>
+              <img
+                alt="pro4"
+                src="https://image.freepik.com/free-photo/elderly-man-is-using-mobile-phone_53876-30130.jpg"
+              />
             </div>
+            <div className="content-style">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Repellat, voluptatibus nisi cum eligendi repudiandae illo aliquid
+              quas ducimus ipsam. Quia?
+            </div>{" "}
           </div>
           <div className="pro-content">
-            <div className="pro-content">
-              <div>Title</div>
-              <div>
-                <img src="https://image.freepik.com/free-photo/keyword-seo-content-website-tags-search_53876-120179.jpg" />
-              </div>
-              <div>content</div>
+            <div className="title-style">Title</div>
+            <div>
+              <img
+                alt="pro5"
+                src="https://image.freepik.com/free-photo/keyword-seo-content-website-tags-search_53876-120179.jpg"
+              />
             </div>
+            <div className="content-style">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Repellat, voluptatibus nisi cum eligendi repudiandae illo aliquid
+              quas ducimus ipsam. Quia?
+            </div>{" "}
           </div>
           <div className="pro-content">
-            <div className="pro-content">
-              <div>Title</div>
-              <div>
-                <img src="https://image.freepik.com/free-photo/coding-man_1098-18084.jpg" />
-              </div>
-              <div>content</div>
+            <div className="title-style">Title</div>
+            <div>
+              <img
+                alt="pro6"
+                src="https://image.freepik.com/free-photo/coding-man_1098-18084.jpg"
+              />
+            </div>
+            <div className="content-style">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Repellat, voluptatibus nisi cum eligendi repudiandae illo aliquid
+              quas ducimus ipsam. Quia?
             </div>
           </div>
         </div>

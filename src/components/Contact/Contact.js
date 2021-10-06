fc8837ac1ffc3fe1eaacec21f8ef421504bd831a
Shahr Zad <gh.shahrzad@gmail.com>
2021-10-06 17:02:29 +0330

    add text to banner

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,21 +4,26 @@ import "./Contact.css";
 function Contact() {
   return (
     <>
-      <div className="banner" style={{ height: "50vh" }}>
-        <img src="https://image.freepik.com/free-photo/web-design-concept-with-drawings_1134-77.jpg" />
+      <div className="banner" style={{ height: "50vh" }} id="contact">
+        <img
+          alt="banner"
+          src="https://image.freepik.com/free-photo/web-design-concept-with-drawings_1134-77.jpg"
+        />
+        <div className="contact-text">CONTACT</div>
       </div>
       <div className="contact">
         <div>
           <img
-            src="https://image.freepik.com/free-photo/web-design-concept-with-drawings_1134-77.jpg"
-            alt=""
+            style={{ width: 400, border: "1px solid gray" }}
+            src="https://image.freepik.com/free-photo/magnifying-glass-map-close-up_155003-3909.jpg"
+            alt="location"
           />
         </div>
         <div>
           <div className="info">
-            <i class="fas fa-map-marker-alt">Karaj</i>
-            <i class="fas fa-phone-alt">+98 9357795479</i>
-            <i class="fas fa-envelope">gh.shahrzad@gmail.com</i>
+            <i className="fas fa-map-marker-alt">Location</i>
+            <i className="fas fa-phone-alt">Phone</i>
+            <i className="fas fa-envelope">Email</i>
           </div>
           <form className="contact-form">
             <input type="email" placeholder="Email"></input>
@@ -29,7 +34,7 @@ function Contact() {
               cols="50"
               placeholder="Message"
             ></textarea>
-            <input type="button" value="SEND"></input>
+            <input type="button" value="SEND MESSAGE"></input>
           </form>
         </div>
       </div>

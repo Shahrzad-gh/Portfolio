22246334c537f488c84356bd722c0c183c5cbdec
Shahr Zad <gh.shahrzad@gmail.com>
2021-10-07 16:48:52 +0330

    add font responsive - media query

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -12,19 +12,19 @@ function Contact() {
         <div className="contact-text">CONTACT</div>
       </div>
       <div className="contact">
-        <div>
+        <div className="center">
           <img
-            style={{ width: 400, border: "1px solid gray" }}
+            className="location-pic"
             src="https://image.freepik.com/free-photo/magnifying-glass-map-close-up_155003-3909.jpg"
             alt="location"
           />
         </div>
+        <div className="info">
+          <i className="fas fa-map-marker-alt">Location</i>
+          <i className="fas fa-phone-alt">Phone</i>
+          <i className="fas fa-envelope">Email</i>
+        </div>
         <div>
-          <div className="info">
-            <i className="fas fa-map-marker-alt">Location</i>
-            <i className="fas fa-phone-alt">Phone</i>
-            <i className="fas fa-envelope">Email</i>
-          </div>
           <form className="contact-form">
             <input type="email" placeholder="Email"></input>
             <input type="text" placeholder="Name"></input>
